2f931f7038b02e0319adfb6f7f0328b43d3c3c30
Rob Scharpf <rscharpf@jhsph.edu>
2012-02-08 09:03:50 -0500

    Remove outdated functions, classes and documentation. Begin transition to lattice methods for visualization

--- R/methods-SnpSet.R
+++
@@ -1,7 +0,0 @@
-setMethod("summary", signature(object = "SnpSet"),
-          function(object, digits = 3, noCalls = FALSE, ...){
-            het <- colMeans(ifelse(calls(object) == 2, 1, 0))
-            hom <- colMeans(ifelse(calls(object) == 1 | calls(object) == 3, 1, 0))
-            mat <- rbind(het, hom)
-            mat
-          })
