1de2137f35e1b23b958b7dcfb147026d6bc7a435
Rob Scharpf <rscharpf@jhsph.edu>
2012-05-15 09:49:43 -0400

    Update SNPchip for compatability with GRanges and GRangesList objects.

--- R/coerce-methods.R
+++ R/coerce-methods.R
@@ -1,12 +1,18 @@
 dataFrameFromRange <- function(range, object, frame, range.index){
-	rm <- IRanges::findOverlaps(range, featureData(object), maxgap=frame) ## RangesMatching
+	if(is(range, "RangedDataCNV")){
+		rm <- IRanges::findOverlaps(range, featureData(object), maxgap=frame) ## RangesMatching
+		sample.index <- match(sampleNames(range), sampleNames(object))
+	} else {
+		frange <- oligoClasses::makeFeatureRanges(object)
+		rm <- IRanges::findOverlaps(range, frange, maxgap=frame)
+		sample.index <- match(sampleNames(range), sampleNames(object))
+	}
+	if(any(is.na(sample.index))) stop("sampleNames in RangedData do not match sampleNames in ", class(data), " object")
+	sample.index <- unique(sample.index)
 	mm <- as.matrix(rm)
 	mm.df <- data.frame(mm)
 	mm.df$featureNames <- featureNames(object)[mm.df$subject]
 	marker.index <- mm.df$subject
-	sample.index <- match(sampleNames(range), sampleNames(object))
-	if(any(is.na(sample.index))) stop("sampleNames in RangedData do not match sampleNames in ", class(data), " object")
-	sample.index <- unique(sample.index)
 	obj <- object[marker.index, sample.index]
 	mm.df$subject <- match(mm.df$featureNames, featureNames(obj))
 	##
@@ -16,6 +22,6 @@ dataFrameFromRange <- function(range, object, frame, range.index){
 	##df$range <- rep(i, nrow(df))##mm.df$query
 	##dfList[[i]] <- df
 	##df$range <- range.index
-	df$range <- paste("[", range.index, "] chr ", chromosome(range), ", ID: ", sampleNames(range), sep="")
+	df$range <- paste("[", range.index, "] chr ", chromosome(range), ", ID: ", sampleNames(obj), sep="")
 	return(df)
 }
